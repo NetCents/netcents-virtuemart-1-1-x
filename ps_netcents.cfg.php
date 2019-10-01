2d7ac8a754919298fc74fce2cc0e9d82c470c7c1
Hayden Carlson <you@example.com>
2019-10-01 15:01:53 -0700

    Add initial plugin files with config

---
+++ ps_netcents.cfg.php
@@ -0,0 +1,7 @@
+<?php
+if( !defined( '_VALID_MOS' ) && !defined( '_JEXEC' ) ) die( 'Direct Access to '.basename(__FILE__).' is not allowed.' ); 
+
+define ('NETCENTS_API_KEY', '');
+define ('NETCENTS_SECRET_KEY', '');
+define ('NETCENTS_GATEWAY', 'https://merchant.net-cents.com');
+?>
